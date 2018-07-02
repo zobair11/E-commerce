e3030e8aeca4382fea5de7fa005d76bfb927638b
zobair11 <zobair619316@gmail.com>
2018-07-02 19:44:25 +0700

    Delete

--- admin/includes/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-ob_start();
-session_start();
-require_once('includes/database.php');
-require_once('includes/functions.php');
--- admin/includes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('init.php');
-
-if( isset($_SESSION['admin_email']) && $_SESSION['admin_email'] != '' ) {
-
-	unset($_SESSION['admin_email']);
-	redirect('login.php');
-	exit();
-}
